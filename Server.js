6a8383840561267e79b989552e49a5b48bc084d4
Alex <olivergaytanl@gmail.com>
2026-05-24 17:23:36 -0600

    Se agrego banner a publicaciones

--- Server.js
+++ Server.js
@@ -334,7 +334,7 @@ app.post('/api/publicaciones', authMiddleware, async (req, res) => {
 app.get('/api/publicaciones', authMiddleware, async (req, res) => {
   const { data, error } = await supabase
     .from('publicaciones')
-    .select('*, users(id, full_name, img_profile)')
+    .select('*, users(id, full_name, img_profile, banner_url)')
     .order('created_at', { ascending: false });
 
   if (error) return res.status(500).json({ error: error.message });
